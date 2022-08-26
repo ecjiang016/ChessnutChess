29612f4a2375b9f42faa932bb8921a2d0b39700e
Evan Jiang <ecjiang016@gmail.com>
2022-08-26 13:04:30 -0500

    Move some functions to game.cpp

--- src/game.cpp
+++ src/game.cpp
@@ -1,2 +1,9 @@
 #include "game.h"
 
+constexpr inline Bitboard Chess::get_bitboard(PieceType piece, Color color) const {
+	return bitboards[makePiece(piece, color)];
+}
+
+std::vector<Piece> Chess::getMailbox() const {
+    return std::vector<Piece>(mailbox, mailbox + 64);
+}
