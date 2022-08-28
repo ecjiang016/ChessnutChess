2fddc3da91341c413668fc5103fe47202a7a69de
ecjiang016 <ecjiang016@gmail.com>
2022-08-27 22:06:57 -0500

    Rewrote perft

--- src/perft.cpp
+++ src/perft.cpp
@@ -1,83 +1,12 @@
-#include "game/game.cpp"
+#include "game.h"
 #include <chrono>
 #include <algorithm> 
 
-int search_captures(int depth, Chess game, Move move) {
-    game.makeMove(move);
-
-    if (depth == 0) {
-        if (move.capture != None) {
-            return 1;
-        } else {
-            return 0;
-        }
-    }
-
-    int checks = 0;
-
-    for (Move move : game.allMoves()) {
-        checks += search_captures(depth-1, game, move);
-    }
-
-    return checks;
-}
-
-int search_captures(int depth, Chess game) {
-    if (depth == 0) {
-        return 0;
-    }
-
-    int checks = 0;
-
-    for (Move move : game.allMoves()) {
-        checks += search_captures(depth-1, game, move);
-    }
-
-    return checks;
-}
-
-int search_checks(int depth, Chess game, Move move) {
-    game.makeMove(move);
-
-    if (depth == 0) {
-        if (game.inCheck()) {
-            return 1;
-        } else {
-            return 0;
-        }
-    }
-
-    int checks = 0;
-
-    for (Move move : game.allMoves()) {
-        checks += search_checks(depth-1, game, move);
-    }
-
-    return checks;
-}
-
-int search_checks(int depth, Chess game) {
-    if (depth == 0) {
-        if (game.inCheck()) {
-            return 1;
-        } else {
-            return 0;
-        }
-    }
-
-    int checks = 0;
-
-    for (Move move : game.allMoves()) {
-        checks += search_checks(depth-1, game, move);
-    }
-
-    return checks;
-}
-
+template<Color color>
 int search(int depth, Chess game, Move move) {
-    game.makeMove(move);
+    game.makeMove<color>(move);
 
-    std::vector<Move> moves = game.allMoves();
+    std::vector<Move> moves = game.getMoves<~color>();
 
     if (depth == 0) {
         return 1;
@@ -86,12 +15,13 @@ int search(int depth, Chess game, Move move) {
     int positions = 0;
 
     for (Move move : moves) {
-        positions += search(depth-1, game, move);
+        positions += search<~color>(depth-1, game, move);
     }
 
     return positions;
 }
 
+template<Color color>
 int search(int depth, Chess game) {
     if (depth == 0) {
         return 1;
@@ -100,8 +30,8 @@ int search(int depth, Chess game) {
     int positions = 0;
     std::vector<std::string> moves_in_pos;
 
-    for (Move move : game.allMoves()) {
-        int move_positions = search(depth-1, game, move);
+    for (Move move : game.getMoves<color>()) {
+        int move_positions = search<~color>(depth-1, game, move);
         positions += move_positions;
         moves_in_pos.push_back(move.UCI() + ": " + std::to_string(move_positions));
     }
@@ -115,21 +45,15 @@ int search(int depth, Chess game) {
 
 int main() {
     Chess game;
-    //game.loadFen("r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1");
-    //Move fm("d2d3", Pawn);
-    //game.makeMove(fm);
+    game.setFen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR");
 
     for (int i = 0; i <= 8; i++) {
         auto start = std::chrono::high_resolution_clock::now();
-        int positions = search(i, game);
-        int checks = search_checks(i, game);
-        int captures = search_captures(i, game);
+        int positions = search<WHITE>(i, game);
         auto end = std::chrono::high_resolution_clock::now();
         std::chrono::duration<double, std::milli> sec = end - start;
         std::cout << std::endl << "Depth: " << i << std::endl;
         std::cout << "Nodes: " << positions << std::endl;
-        std::cout << "Checks: " << checks << std::endl;
-        std::cout << "Captures: " << captures << std::endl;
-        std::cout << std::fixed << (positions / (sec.count() / 1000.0)) * 3 << " nps" << std::endl << std::endl;
+        std::cout << std::fixed << (positions / (sec.count() / 1000.0)) << " nps" << std::endl << std::endl;
     }
 }
